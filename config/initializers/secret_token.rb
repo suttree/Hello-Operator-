80e988016867dc90571dadd8e593f7a3e95cf6e4
Duncan Gough <duncan@suttree.com>
2011-10-29 21:15:33 +0100

    Hello, Operator?

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HelloOperator::Application.config.secret_token = '2c58fa22de774d0389a97506789b302b3c01f779532c4fd799a6432639a2e5cffee20f01f307748955ed94f45d57cfa3ce837ba88748b55724e8f275fda95ddd'
